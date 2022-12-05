ee58cca333c76b03845ded13dcb4dcbe5534aa31
tellgreenpo <mateo1000a@gmail.com>
2022-12-05 15:15:02 +0100

    Added p value check

--- exploratory.py
+++ exploratory.py
@@ -1,8 +1,10 @@
 import pandas as pd
-import csv
 import matplotlib.pyplot as plt
+from statsmodels.tsa.stattools import adfuller
 
 nodes = ['A','B','C']
+
+# 3 seconds lag
 
 def clean(file):
     df = pd.read_csv(file,sep=';')
@@ -13,8 +15,6 @@ def clean(file):
 def write_csv(df):
     df.to_csv('./RAM_CPU_value.csv')
 
-df = clean('./value.csv')
-
 def plot(df,node):
     ram = []
     cpu = []
@@ -22,8 +22,21 @@ def plot(df,node):
         ram = df['RAM_{0}'.format(node)]
         cpu = df['CPU_{0}'.format(node)]
     plt.plot(ram[:100])
-    plt.show() 
+    plt.show()
 
-plot(df,'A')
-print(df['RAM_A'].mean())
-print(df['RAM_A'].var())
+
+def dickey_fuller_test(series):
+    fuller = adfuller(series,autolag='AIC')
+    return fuller[1]
+
+def print_p_values(df,nodes):
+    for node in nodes:
+        pValueRam = dickey_fuller_test(df['RAM_{0}'.format(node)].values)
+        pValueCpu = dickey_fuller_test(df['CPU_{0}'.format(node)].values)
+        print('RAM_{0} p value: {1}'.format(node,pValueRam))
+        print('CPU_{0} p value: {1}'.format(node,pValueCpu))
+
+
+
+df = clean('./value.csv')
+print_p_values(df,nodes)
