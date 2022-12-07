31dd01458bea1d2acf9f0a3b548a8f287d94255b
tellgreenpo <mateo1000a@gmail.com>
2022-12-07 16:57:29 +0100

    arima model training module added

--- arima.py
+++ arima.py
@@ -4,6 +4,18 @@ from itertools import product
 from tqdm.notebook import tqdm
 
 
+#Set initial values and some bounds
+ps = range(0, 5)
+d = 1
+qs = range(0, 5)
+Ps = range(0, 5)
+D = 1
+Qs = range(0, 5)
+s = 5
+
+#Create a list with all possible combinations of parameters
+parameters = product(ps, qs, Ps, Qs)
+parameters_list = list(parameters)
 
 
 # Train many SARIMA models to find the best set of parameters
@@ -11,9 +23,10 @@ def optimize_SARIMA(df,targetFeature,parameters_list, d, D, s):
 
     results = []
     best_aic = float('inf')
-
+    print(len(parameters_list))
+    print(df.CPU_A)
     for param in tqdm(parameters_list):
-        try: model = sm.tsa.statespace.SARIMAX(df[targetFeature], order=(param[0], d, param[1]),
+        try: model = sm.tsa.statespace.SARIMAX(df.CPU_A, order=(param[0], d, param[1]),
                                                seasonal_order=(param[2], D, param[3], s)).fit(disp=-1)
         except:
             continue
@@ -27,7 +40,9 @@ def optimize_SARIMA(df,targetFeature,parameters_list, d, D, s):
             best_param = param
         results.append([param, model.aic])
 
+    print(len(results))
     result_table = pd.DataFrame(results)
+    print(result_table.head())
     result_table.columns = ['parameters', 'aic']
     #Sort in ascending order, lower AIC is better
     result_table = result_table.sort_values(by='aic', ascending=True).reset_index(drop=True)
@@ -38,25 +53,14 @@ def optimize_SARIMA(df,targetFeature,parameters_list, d, D, s):
 
 
 def compute(df,targetFeature):
-    #Set initial values and some bounds
-    ps = range(0, 5)
-    d = 1
-    qs = range(0, 5)
-    Ps = range(0, 5)
-    D = 1
-    Qs = range(0, 5)
-    s = 5
 
-    #Create a list with all possible combinations of parameters
-    parameters = product(ps, qs, Ps, Qs)
-    parameters_list = list(parameters)
-    len(parameters_list)
+    print(len(parameters_list))
 
     result_table = optimize_SARIMA(df,targetFeature,parameters_list, d, D, s)
     p, q, P, Q = result_table.parameters[0]
 
     # Fit the model with the best parameters
-    best_model = sm.tsa.statespace.SARIMAX(df[targetFeature], order=(p, d, q),
+    best_model = sm.tsa.statespace.SARIMAX(df.CPU_A, order=(p, d, q),
                                         seasonal_order=(P, D, Q, s)).fit(disp=-1)
 
     return best_model
--- main.py
+++ main.py
@@ -6,7 +6,7 @@ def test():
     df = pd.read_csv('RAM_CPU_value.csv',sep=',')
     df.rename(columns= {df.columns[0] : "Index"},inplace=True)
     # Test for CPU_A
-    shortDf = df['CPU_A'][:200]
+    shortDf = df.iloc[:100,]
     model = arima.compute(shortDf,'CPU_A')
     print(model.summary())
 
